13481b2b1b71fc4b7c58005fe2a29e5f95ef3f90
Christian Peña <ch.penaf@gmail.com>
2024-08-17 11:56:04 -0400

    SAPUI5 OPA Tests

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["cpf/com/sap/training/ux402/opa/ux402opa/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
